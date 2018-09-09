d37192671ceb21c2bd248f1ad58c856af9115422
yougahee <dbrkgml97@nate.com>
2018-09-09 16:51:11 +0900

    foldering

--- app/src/main/java/com/naengjjambbong/hankangmoa/Jemin/Activity/MainActivity.kt
+++ app/src/main/java/com/naengjjambbong/hankangmoa/Jemin/Activity/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.naengjjambbong.hankangmoa
+package com.naengjjambbong.hankangmoa.Jemin.Activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.naengjjambbong.hankangmoa.R
 
 class MainActivity : AppCompatActivity() {
 
+    // start
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
