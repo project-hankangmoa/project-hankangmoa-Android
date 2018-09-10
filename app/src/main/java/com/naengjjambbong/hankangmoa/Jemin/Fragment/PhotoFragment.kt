016dadb676817bd99c09772296a785663850ae46
SongJemin <jemin56@naver.com>
2018-09-10 15:31:58 +0900

    Jemin merge and New Start

---
+++ app/src/main/java/com/naengjjambbong/hankangmoa/Jemin/Fragment/PhotoFragment.kt
@@ -0,0 +1,20 @@
+package com.naengjjambbong.hankangmoa.Jemin.Activity.Fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.naengjjambbong.hankangmoa.R
+
+class PhotoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val v = inflater.inflate(R.layout.fragment_photo, container, false)
+        // Inflate the layout for this fragmen
+
+        return v
+    }
+}
