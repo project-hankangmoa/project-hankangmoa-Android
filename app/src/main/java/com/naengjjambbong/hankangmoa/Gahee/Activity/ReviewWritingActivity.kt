7295c49dc95a86681b9df42c2d7fd45d0f82b376
yougahee <dbrkgml97@nate.com>
2018-09-22 01:34:05 +0900

    Merge branch 'master' of https://github.com/project-hankangmoa/project-hankangmoa-Android into gahee
    # Conflicts:
    #	.gradle/4.4/fileHashes/fileHashes.bin
    #	.gradle/4.4/fileHashes/fileHashes.lock
    #	.gradle/4.4/javaCompile/classAnalysis.bin
    #	.gradle/4.4/javaCompile/javaCompile.lock
    #	.gradle/4.4/javaCompile/taskHistory.bin
    #	.gradle/4.4/javaCompile/taskJars.bin
    #	.gradle/4.4/taskHistory/taskHistory.bin
    #	.gradle/4.4/taskHistory/taskHistory.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/caches/build_file_checksums.ser
    #	app/app.iml

--- app/src/main/java/com/naengjjambbong/hankangmoa/Gahee/Activity/ReviewWritingActivity.kt
+++ app/src/main/java/com/naengjjambbong/hankangmoa/Gahee/Activity/ReviewWritingActivity.kt
@@ -1,20 +1,32 @@
 package com.naengjjambbong.hankangmoa.Gahee.Activity
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.RatingBar
-import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.naengjjambbong.hankangmoa.Gahee.Adapter.MypageSteamListAdapter
 import com.naengjjambbong.hankangmoa.Gahee.Item.MypageSteamListItem
 import com.naengjjambbong.hankangmoa.R
-import kotlinx.android.synthetic.main.activity_review.*
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.activity_review_writting.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 
 class ReviewWritingActivity : AppCompatActivity() {
@@ -25,6 +37,11 @@ class ReviewWritingActivity : AppCompatActivity() {
 
     //lateinit var complete_button : Button
     lateinit var star_rating_bar : RatingBar
+
+
+    private val REQ_CODE_SELECT_IMAGE = 100
+    lateinit var data : Uri
+    private var image : MultipartBody.Part? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +74,76 @@ class ReviewWritingActivity : AppCompatActivity() {
         //projectItems.add(ProjectItem("https://project-cowalker.s3.ap-northeast-2.amazonaws.com/1531113346984.jpg", "ㅁㄴㅇㅎ", "ㅁㄴㅇㄹㄴㅁㅇㅎ", "ㅁㄴㅇㄹ", "ㅇㅎㅁㄴㅇㄹ"))ㅇ
         mypageSteamListAdapter = MypageSteamListAdapter(mypageSteamListItem, requestManager)
 
+        val view = window.decorView
+
+        review_write_add_img.setOnClickListener {
+            changeImage()
+        }
+
     }
 
+    fun changeImage(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+        intent.data = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        startActivityForResult(intent, REQ_CODE_SELECT_IMAGE)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQ_CODE_SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    //if(ApplicationController.getInstance().is)
+                    this.data = data!!.data
+                    Log.v("이미지", this.data.toString())
+
+                    val options = BitmapFactory.Options()
+
+                    var input: InputStream? = null // here, you need to get your context.
+                    try {
+                        input = applicationContext.contentResolver.openInputStream(this.data)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+
+                    val bitmap = BitmapFactory.decodeStream(input, null, options) // InputStream 으로부터 Bitmap 을 만들어 준다.
+                    val baos = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos)
+                    val photoBody = RequestBody.create(MediaType.parse("image/jpg"), baos.toByteArray())
+                    val img = File(getRealPathFromURI(applicationContext,this.data).toString()) // 가져온 파일의 이름을 알아내려고 사용합니다
+
+                    Log.v("TAG","이미지 이름 = " + img)
+                    Log.v("TAG","이미지 바디 = " + photoBody.toString())
+
+
+                    image = MultipartBody.Part.createFormData("image", img.name, photoBody)
+
+                    //body = MultipartBody.Part.createFormData("image", photo.getName(), profile_pic);
+
+                    Glide.with(this)
+                            .load(data.data)
+                            .centerCrop()
+                            .into(register_profile_img)
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+
+    fun getRealPathFromURI(context: Context, contentUri: Uri): String {
+        var cursor: Cursor? = null
+        try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor = context.contentResolver.query(contentUri, proj, null, null, null)
+            val column_index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor.moveToFirst()
+            return cursor.getString(column_index)
+        } finally {
+            cursor?.close()
+        }
+    }
 }
